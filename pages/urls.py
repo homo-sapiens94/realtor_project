12120567414533ebdc9fd6d30c84a1011c905af2
Jagat Singh <jagat121994@gmail.com>
2019-09-17 21:30:46 +0530

    static files and linking

--- pages/urls.py
+++ pages/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+
 ]
-
-
-
 
